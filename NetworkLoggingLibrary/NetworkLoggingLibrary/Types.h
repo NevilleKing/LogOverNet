dce4cd32f5e94b566ff77c2a0c3d6d1aa3706d28
Neville King <nevster07@gmail.com>
2017-04-07 23:57:11 +0100

    Added some typedefs for basic types
    Can now use lon_int instead of LonType<LonBasicType<int>>.

--- NetworkLoggingLibrary/NetworkLoggingLibrary/Types.h
+++ NetworkLoggingLibrary/NetworkLoggingLibrary/Types.h
@@ -9,6 +9,14 @@
 
 namespace logovernet
 {
+	// typedefs for basic types
+	typedef LonType<LonBasicType<bool>>    lon_bool;
+	typedef LonType<LonBasicType<char>>    lon_char;
+	typedef LonType<LonBasicType<int>>     lon_int;
+	typedef LonType<LonBasicType<float>>   lon_float;
+	typedef LonType<LonBasicType<double>>  lon_double;
+	typedef LonType<LonBasicType<wchar_t>> lon_wchar_t;
+
 	// helper for copy constructor below
 	template<bool B, typename T = void> using disable_if = std::enable_if<!B, T>;
 
